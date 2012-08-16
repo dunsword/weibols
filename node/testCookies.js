4e003d9bc0c41862211cb8eece29abe16a4a2438
Paul Wang <wagzhi@gmail.com>
2012-08-16 08:32:53 +0800

    test

--- node/testCookies.js
+++ node/testCookies.js
@@ -1,5 +1,6 @@
 var mongoose=require('mongoose');
 var cookies=require('./cookies');
+<<<<<<< HEAD
 var jsunit=require('./jsunit').jsunit;
 var test=new jsunit();
 
@@ -11,11 +12,26 @@ test.setup=function(callback){
 }
 
 test.clean=function(){
+=======
+
+
+function setup(){
+	console.log('this is test');
+	var dbUrl='mongodb://localhost/weibols';
+	mongoose.connect(dbUrl);
+}
+
+function clean(){
+>>>>>>> c9abaa22cac1c39a5adcaabb8f02b0fba050664d
 	mongoose.disconnect();
 	console.log('clean up');
 }
 
+<<<<<<< HEAD
 test.testSaveCookies=function(callback){
+=======
+function testSaveCookies(callback){
+>>>>>>> c9abaa22cac1c39a5adcaabb8f02b0fba050664d
 	console.log('test save cookies');
 
 	var cookieString='uname=Paul;Domain=19lou.com; Expires=Thu, 01-Jan-1970 00:00:10 GMT; Path=/';
@@ -27,24 +43,44 @@ test.testSaveCookies=function(callback){
 		test.assert(false,message);
 	}
 	callback();
+<<<<<<< HEAD
 	});
 
 }
 
 
 test.testGetCookies=function(callback){
+=======
+});
+
+}
+
+function testGetCookies(callback){
+>>>>>>> c9abaa22cac1c39a5adcaabb8f02b0fba050664d
 	console.log('test get cookies');
 	callback();
 }
 
+<<<<<<< HEAD
 test.testCleanCookies=function(callback){
+=======
+function testCleanCookies(callback){
+>>>>>>> c9abaa22cac1c39a5adcaabb8f02b0fba050664d
 	console.log('test clean cookies');
 	callback();
 }
 
+<<<<<<< HEAD
 test.dd=function(dd){}
 
 //var tests=[setup,testSaveCookies,testGetCookies,testCleanCookies,clean];
 //test.runtest(tests);
 test.run();
 
+=======
+var tests=[testSaveCookies,testGetCookies,testCleanCookies];
+
+exports.tests=tests;
+exports.setup=setup;
+exports.clean=clean;
+>>>>>>> c9abaa22cac1c39a5adcaabb8f02b0fba050664d
